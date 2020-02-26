569850de31eca820acdfc41e6b053a7d298f8aea
starhn87 <starhn87@gmail.com>
2020-02-26 11:24:42 +0900

    gps 추가 및 마커 생성

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -7,30 +7,60 @@ document.addEventListener("DOMContentLoaded", () => {
     //let a = map.initMap();
 })
 
+let map;
+let infoWindow;
 function initMap() {
-    let map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 35.050725, lng: 128.978905 },
-        zoom: 16
+        zoom: 14
     });
+
+
+    infoWindow = new google.maps.InfoWindow;
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            let pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            map.setCenter(pos);
+            let latLng = new google.maps.LatLng(pos.lat, pos.lng);
+            let marker = new google.maps.Marker({
+                position: latLng, //여기에 위도 경도 정보를 입력하고 마커 생성
+                map: map,
+            });
+            marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+            infowindow.open(map, marker);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
     fetch("http://localhost:8080/shelter/Marker").then((receive) => {
         receive.json().then((results) => {
             console.log(results);
             for (let i = 0; i < results.datas.length; i++) {
-                let contentString="<div>"+results.datas[i].name+"</div>";
-                contentString+="<div>"+results.datas[i].jibun+"</div>"
-
-                let latLng = new google.maps.LatLng(results.datas[i].coordinates[0], results.datas[i].coordinates[1]); //위도 경도 변수
-                let infowindow = new google.maps.InfoWindow({
+                let contentString = "<div>" + results.datas[i].name + "</div>";
+                infowindow = new google.maps.InfoWindow({
                     content: contentString,
                     maxWidth: 300
                 });
+                contentString += "<div>" + results.datas[i].jibun + "</div>"
+
+                let latLng = new google.maps.LatLng(results.datas[i].coordinates[0], results.datas[i].coordinates[1]); //위도 경도 변수
 
                 let marker = new google.maps.Marker({
                     position: latLng, //여기에 위도 경도 정보를 입력하고 마커 생성
                     map: map,
                     id: results.datas[i].id
                 });
-                marker.addListener('click', function() {
+                marker.addListener('click', function () {
                     infowindow.open(map, marker);
                 });
             }
@@ -38,12 +68,11 @@ function initMap() {
     })
 }
 
-/*let map = {
-    map: null,
-    initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: 35.050725, lng: 128.978905 },
-            zoom: 16
-        });
-    }
-}*/
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+
